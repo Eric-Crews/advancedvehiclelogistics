805692270af3b1e55853ab400483c0f93fb10fb5
Eric Crews <137831447+Eric-Crews@users.noreply.github.com>
2026-09-24 22:37:11 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "9b8d8966-8b91-4ca4-96f8-69d07884b646";
 
 const { d1, r2 } = hostingConfig;
 
